e4cf109230497ee32fc0e98415b249f7f2a0780a
Vandith Reddy <71482642+VandithReddy@users.noreply.github.com>
2022-07-28 10:00:45 +0530

    Add files via upload

--- capapp.py
+++ capapp.py
@@ -1,14 +1,35 @@
-import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-my_dataset="WA_Fn-UseC_-Telco-Customer-Churn.csv"
+import warnings
+import streamlit as st
+warnings.filterwarnings('ignore')
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn import metrics
+from sklearn import model_selection
+from sklearn.metrics import roc_curve
+from sklearn.metrics import recall_score, confusion_matrix, precision_score, f1_score, accuracy_score, classification_report
+my_dataset="Telco-Customer-Churn_21para.csv"
 def explore_data(dataset):
     df=pd.read_csv(dataset)
     return df
 data=explore_data(my_dataset)
-features=19
+features_n=19
 st.set_page_config(initial_sidebar_state="collapsed")
 html_temp = """
     <div style="background-color:grey;padding:10px">
@@ -74,7 +95,7 @@ def DataVisualization():
             st.write(sns.distplot(data[column_name]))
             st.pyplot()
         elif column_name=="TotalCharges":
-            plt.figure(figsize=(5,3))
+            plt.figure(figsize=(4,3))
             st.set_option('deprecation.showPyplotGlobalUse', False)
             st.write(plt.hist(data[column_name],color="skyblue",edgecolor="black"))
             st.pyplot()
@@ -100,8 +121,36 @@ def DataVisualization():
             st.write(plt.hist(data[column_name],color="skyblue",edgecolor="black"))
             st.pyplot()
 def prediction():
+    data = pd.read_csv("Telco-Customer-Churn_21para.csv")
+    data.head()
+    data = data.drop(["customerID"], axis = 1)
+    data.drop(labels=data[data["TotalCharges"] == ' '].index,axis=0,inplace=True)
+    data = data.reset_index(drop = True)
+    data['TotalCharges'] = pd.to_numeric(data.TotalCharges, errors='coerce')
+    data.SeniorCitizen = data.SeniorCitizen.map({0: "No", 1: "Yes"})
+    cat=[]
+    num=[]
+    for i in data.columns:
+        if(data[i].dtype=="object"):
+            cat.append(i)
+        else:
+            num.append(i)
+    data['Churn'] = data['Churn'].astype('object')
+    y=data['Churn']
+    # y=y.astype('int')
+    cat.remove('Churn')
+    data_encoded=data.drop('Churn',axis=1)
+    data_encoded.head()
+    cat_data = pd.get_dummies(data_encoded[cat])
+    scaler = StandardScaler()
+    features = data_encoded[num]
+    features = scaler.fit_transform(features)
+    num_features=pd.DataFrame(features)
+    attributes = pd.concat([cat_data, num_features], axis=1)
+    X=attributes
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state = 1)
     st.header("Prediction")
-    col_list=[0]*features
+    col_list=[0]*features_n
 
     gen_col=["Female","Male"]
     gen_num=list(range(len(gen_col)))
@@ -111,7 +160,7 @@ def prediction():
     age_col=[0,1]
     age_num=list(range(len(age_col)))
     age=st.radio("Senior Citizen?",age_num,format_func=lambda x:age_col[x])
-    col_list[1]=age_col
+    col_list[1]=age
 
     partner_col=["Yes","No"]
     partner_num=list(range(len(partner_col)))
@@ -123,83 +172,79 @@ def prediction():
     dpdt=st.radio("Dependant?",dpdt_num,format_func=lambda x:dpdt_col[x])
     col_list[3]=dpdt
 
-    Tenure_col=st.slider("Enter tenure : ",0,72)
-    col_list[4]=Tenure_col
-
     ps_col=["Yes","No"]
     ps_num=list(range(len(ps_col)))
     ps=st.radio("Phone Service?",ps_num,format_func=lambda x:ps_col[x])
-    col_list[5]=ps
+    col_list[4]=ps
 
     ml_col=["Yes","No"]
     ml_num=list(range(len(ml_col)))
     ml=st.radio("Multiple Lines?",ml_num,format_func=lambda x:ml_col[x])
-    col_list[6]=ml
+    col_list[5]=ml
 
-    InternetService=["InternetService","Fiber Optic","DSL","No internet service"]
-    InternetS_option=st.selectbox("",InternetService)
-    if InternetS_option=="Fiber Optic":
-        col_list[7]="Fiber Optic"
+    # InternetService=["InternetService","Fiber Optic","DSL","No internet service"]
+    # InternetS_option=st.selectbox("",InternetService)
+    # if InternetS_option=="Fiber Optic":
+    #     col_list[7]=InternetS_option
+    #     # col_list[10]=0
+    # elif InternetS_option=="DSL":
+    #     col_list[7]=InternetS_option
+    #     # col_list[10]=1
+    # elif InternetS_option=="No internet service":
+    #     col_list[7]=InternetS_option
         # col_list[10]=0
-    elif InternetS_option=="DSL":
-        col_list[7]="DSL"
-        # col_list[10]=1
-    elif InternetS_option=="No internet service":
-        col_list[7]="No internet service"
-        # col_list[10]=0
+    InternetService=["Fiber Optic","DSL","No internet service"]
+    IS=list(range(len(InternetService)))
+    ise=st.radio("Choose Your Internet Service Type:",IS,format_func=lambda x:InternetService[x])
+    col_list[6]=ise
     
     os_col=["Yes","No"]
     os_num=list(range(len(os_col)))
     os=st.radio("Online Security?",os_num,format_func=lambda x:os_col[x])
-    col_list[8]=os
+    col_list[7]=os
 
     ob_col=["Yes","No"]
     ob_num=list(range(len(ob_col)))
     ob=st.radio("Online Backup?",ob_num,format_func=lambda x:ob_col[x])
-    col_list[9]=ob
+    col_list[8]=ob
 
     dp_col=["Yes","No"]
     dp_num=list(range(len(dp_col)))
     dp=st.radio("Device Protection?",dp_num,format_func=lambda x:dp_col[x])
-    col_list[10]=dp
+    col_list[9]=dp
 
     ts_col=["Yes","No"]
     ts_num=list(range(len(ts_col)))
     ts=st.radio("Tech Support?",ts_num,format_func=lambda x:ts_col[x])
-    col_list[11]=ts
+    col_list[10]=ts
 
     stv_col=["Yes","No"]
     stv_num=list(range(len(stv_col)))
     stv=st.radio("Streaming TV?",stv_num,format_func=lambda x:stv_col[x])
-    col_list[12]=stv
+    col_list[11]=stv
 
     sm_col=["Yes","No"]
     sm_num=list(range(len(sm_col)))
     sm=st.radio("Streaming Movie?",sm_num,format_func=lambda x:sm_col[x])
-    col_list[13]=sm
+    col_list[12]=sm
 
-    Contract=["Contract","One year","month-to-month"]
-    Contract_option=st.selectbox("",Contract)
-    if Contract_option=="One year":
-        col_list[14]="One year"
-    elif InternetS_option=="month-to-month":
-        col_list[14]="month-to-month"
+    Contract=["One year","month-to-month"]
+    con_num=list(range(len(Contract)))
+    con=st.radio("Choose your Contract Type?",con_num,format_func=lambda x:Contract[x])
+    col_list[13]=con
 
     pbl_col=["Yes","No"]
     pbl_num=list(range(len(pbl_col)))
     pbl=st.radio("Paper Billing?",pbl_num,format_func=lambda x:sm_col[x])
-    col_list[15]=pbl
+    col_list[14]=pbl
 
-    paymentmethod=["Payment Method","Electronic check","Mail check","Credit card (automatic)","Bank transfer (automatic)"]
-    pm_option=st.selectbox("",paymentmethod)
-    if pm_option=="Electronic check":
-        col_list[16]="Electronic check"
-    elif pm_option=="Mail check":
-        col_list[16]="Mail check"
-    elif pm_option=="Credit card (automatic)":
-        col_list[16]="Credit card (automatic)"
-    elif pm_option=="Bank transfer (automatic)":
-        col_list[16]="Bank transfer (automatic)"
+    paymentmethod=["Electronic check","Mail check","Credit card (automatic)","Bank transfer (automatic)"]
+    pm_option=list(range(len(paymentmethod)))
+    pm=st.radio("Payment Method?",pm_option,format_func=lambda x:paymentmethod[x])
+    col_list[15]=pm
+
+    Tenure_col=st.slider("Enter tenure : ",0,72)
+    col_list[16]=Tenure_col
 
     Monthlycharges=st.number_input("Enter Monthly charges: ",step=0.01)
     col_list[17]=Monthlycharges
@@ -208,7 +253,7 @@ def prediction():
     col_list[18]=Totalcharges
 
 
-
+    st.write(col_list)
     if st.checkbox("Your entries"):
         d={}
         feature=["gender","SeniorCitizen","Partner","Dependents","tenure","PhoneService","MultipleLines","InternetService","OnlineSecurity","OnlineBackup","DeviceProtection","TechSupport","StreamingTV","StreamingMovies","Contract","PaperlessBilling","PaymentMethod","MonthlyCharges","TotalCharges"]
@@ -220,7 +265,195 @@ def prediction():
         st.write(d)
 
     if st.button("Predict"):
-        pass
+        original=[]
+        if(col_list[0]==0):
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(1)
+        if(col_list[1]==0):
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(1)
+
+        if(col_list[2]==0):
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(1)
+
+        if(col_list[3]==0):
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(1)
+
+        if(col_list[4]==0):
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(1)
+
+        if(col_list[5]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[5]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[6]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[6]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[7]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[7]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[8]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[8]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[9]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[9]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[10]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[10]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[11]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[11]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[12]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[12]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[13]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[13]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(1)
+
+        if(col_list[14]==0):
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(1)
+        # 'PaymentMethod_Bank transfer (automatic)','PaymentMethod_Credit card (automatic)','PaymentMethod_Electronic check','PaymentMethod_Mailed check',
+
+        if(col_list[15]==0):
+            original.append(1)
+            original.append(0)
+            original.append(0)
+            original.append(0)
+        elif(col_list[15]==1):
+            original.append(0)
+            original.append(1)
+            original.append(0)
+            original.append(0)
+        elif(col_list[15]==2):
+            original.append(0)
+            original.append(0)
+            original.append(1)
+            original.append(0)
+        else:
+            original.append(0)
+            original.append(0)
+            original.append(0)
+            original.append(1)
+        original.append(col_list[16])
+        original.append(col_list[17])
+        original.append(col_list[18])
+        classifier = LogisticRegression(random_state=0)
+        classifier.fit(X_train, y_train)
+        # original2.reshape(1,-1)
+        # scaler = MinMaxScaler()
+        y_pred = classifier.predict([original])
+        st.write(y_pred)
 def statistics():
     pass
 def about():
